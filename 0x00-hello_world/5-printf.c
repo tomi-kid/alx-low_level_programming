76cad1f90e56d6ecd5aa396545b14cfd70d8e870
tomas_ketema <tomiketema209@gmail.com>
2022-12-08 04:11:05 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ *  main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 
 int main(void)
 
