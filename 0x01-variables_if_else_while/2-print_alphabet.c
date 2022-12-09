267d7d7511861ff57b861c1d7aef01e0af6665a7
tomi-kid <tomiketema209@gmail.com>
2022-12-09 08:59:28 -0800

    C - Variables, if, else, while From task 2 to 9

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
-
 /**
    * main - program that prints the alphabet in lowercase, followed by a new lin
-
     * You can only use the "putchar"
     * Return: 0
     */
-
 int main(void)
 {
 	int l = 'a';
@@ -19,3 +16,5 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
+
