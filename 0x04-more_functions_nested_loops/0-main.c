6135f2bd599a8a49d1fbb026a67936c2086d12a1
tomi-kid <tomiketema209@gmail.com>
2022-12-15 06:50:47 -0800

    test task 1

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char c;
+	int c;
 
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	_isupper(c);
 	return (0);
 }
