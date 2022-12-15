77175983cfa27034096570ad73fe25f6546f1516
tomi-kid <tomiketema209@gmail.com>
2022-12-15 03:45:06 -0800

    Task one file added positive_or_negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * positive_or_negative - check postive or negative number
+  * Return: 0 (Sucess)
+  */
+
+void positive_or_negative(int i)
+
+{
+	if (i <= 0.0)
+	{
+		if (i == 0.0)
+			printf("You entered 0.");
+		else
+			printf("%d is negative", i);
+	}
+	else
+		printf("%d is positive", i);
+
+}
