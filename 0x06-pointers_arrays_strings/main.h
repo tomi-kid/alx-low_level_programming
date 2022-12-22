b776b83f95a2b80cb3ddb4a6a109677222844049
tomi-kid <tomiketema209@gmail.com>
2022-12-22 05:43:00 -0800

    advanced tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,12 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+
 
 #endif
