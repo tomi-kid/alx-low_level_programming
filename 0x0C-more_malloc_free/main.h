2b75dc0fc4346b78e69fb5a39d3c410b4cb44dbd
tomi-kid <tomiketema209@gmail.com>
2023-01-12 07:20:15 -0800

    malloc and calloc tasks

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
