e649579a058827beb42d2284330e97c0a54f52d4
tomi-kid <tomiketema209@gmail.com>
2023-01-16 04:11:30 -0800

    function defination for struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - structure that store dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
