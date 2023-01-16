eb659540cc41fd54ab4f116efe598d8821ae553e
tomi-kid <tomiketema209@gmail.com>
2023-01-16 05:04:41 -0800

    All test commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure that store dog information
  * @name: dog name
  * @age: dog age
@@ -13,4 +18,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
