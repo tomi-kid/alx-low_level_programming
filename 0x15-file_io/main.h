6c0b23430fbb379caa1b202c595f4122452b0f60
tomas <tomiketema209@gmail.com>
2023-02-07 00:22:05 -0800

    ADVANCED task display ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,5 @@
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 #endif
