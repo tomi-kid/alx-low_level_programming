9906ae50aac0cb41eedee94179b195e4864e45fa
tomas <tomiketema209@gmail.com>
2023-02-07 00:16:01 -0800

    File input output C

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+#endif
