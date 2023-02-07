6ba6b28f9b21706b7c8c362d077af4d666008419
tomas <tomiketema209@gmail.com>
2023-02-07 00:27:53 -0800

    Fix 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
