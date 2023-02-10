c5a23ecac7523a8269d51ff3ce70c87184d8bc94
tomas <tomiketema209@gmail.com>
2023-02-09 23:17:23 -0800

    Fix Task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
