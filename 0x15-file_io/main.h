1d09f2c4e2ef72330180452eb41b0dde049e235e
tomi-kid <tomiketema209@gmail.com>
2023-02-23 10:31:11 -0800

    test1

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+int _putchar(char c);
+
+#endif
