b4ae5b7c7c11753e382053120f55d0505075b3db
tomi-kid <tomiketema209@gmail.com>
2023-03-20 10:08:53 -0700

    Test

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _isupper - checks if parameter is an uppercase character.
+  * @c: input character.
+  * Return: 1 if is an uppercase character, 0 in other case.
+  */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		{
+		return (0);
+		}
+		else
+		{
+		return (1);
+		}
+}
